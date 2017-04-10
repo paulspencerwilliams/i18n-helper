0aa850e8fb7300789b50c078e36b788835537537
Paul Williams <paul@paulswilliams.me.uk>
2017-04-10 19:47:04 +0100

    Updated for summary and link to github page

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject i18n-helper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small utility to help wrangle message.properties files"
+  :url "https://github.com/paulspencerwilliams/i18n-helper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
