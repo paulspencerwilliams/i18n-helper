7ebfe429bbd476408acb877ee47d9c2aeece19a5
Paul Williams <paul@paulswilliams.me.uk>
2017-04-17 17:12:06 +0100

    upgraded to Clojure 1.9 alpha to receive Spec

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/paulspencerwilliams/i18n-helper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/tools.cli "0.3.5"]]
   :plugins [[lein-bin "0.3.4"]]
   :bin { :name "i18n-helper" }
