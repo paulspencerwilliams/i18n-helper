5c20c2e4f34fad9f4521e7555cc7bb4f57b30574
Paul Williams <paul@paulswilliams.me.uk>
2017-04-17 17:34:50 +0100

    initial spec for existent directory at messages-path

--- src/i18n_helper/newcore.clj
+++ src/i18n_helper/newcore.clj
@@ -1,6 +1,11 @@
-(ns i18n-helper.newcore)
+(ns i18n-helper.newcore
+  (:require [clojure.spec :as s]
+            [clojure.java.io :as io]))
+
+(s/def ::messages-path (s/and string? #(.exists (io/file %))  #(.isDirectory (io/file %))))
 
 (defn report-missing [messages-path]
+  {:pre [(s/valid? ::messages-path messages-path)]}
   (println (str "Reporting missing for resources in '" messages-path  "'")))
 
 (defn report-translations-pending []
